8d867345e4ff7d8ea01679f7aa1c0ede605826f5
VTaPo <vtphong82@gmail.com>
2024-03-19 15:11:32 +0700

    get KGEs faster with mapping files

--- fairseq/FB15K_KB/semantic_sim.py
+++ fairseq/FB15K_KB/semantic_sim.py
@@ -1,9 +1,10 @@
 from sentence_transformers import util
 
 def semanticSim(origin_text,compare_text,model):
-	embedding_1= model.encode(origin_text, convert_to_tensor=True)
-	embedding_2 = model.encode(compare_text, convert_to_tensor=True)
-	return util.pytorch_cos_sim(embedding_1, embedding_2)
+	embedding_1= model.encode(origin_text, convert_to_tensor=True, show_progress_bar=False)
+	embedding_2 = model.encode(compare_text, convert_to_tensor=True, show_progress_bar=False)
+	score = util.pytorch_cos_sim(embedding_1, embedding_2)[0][0].item()
+	return score
 
 def get_maxIndex_semanticSim(origin_text,list_text,model):
 	scores = list(map(lambda text: semanticSim(origin_text, text, model), list_text))
